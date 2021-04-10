25e04941dc54c7539df65c70a5ad8f5f36fda25e
Bilal Abouzid <belal246532@gmail.com>
2021-04-10 01:18:32 +0100

    New Design

---
+++ public/helper.js
@@ -0,0 +1,10 @@
+function showHidePass() {
+    const passInput = document.getElementById("Pass");
+    const confPassInput = document.getElementById('confPass');
+
+    (passInput.type === 'password')? passInput.type = 'text' : passInput.type = 'password';
+
+    if(confPassInput !== null){
+        (confPassInput.type === 'password')? confPassInput.type = 'text' : confPassInput.type = 'password';
+    }
+};
